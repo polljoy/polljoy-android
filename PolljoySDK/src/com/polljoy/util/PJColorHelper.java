74a583c05cbc2a9575e5d7fbbeef94c9c5eef3de
James Zhao <jameszhao1116@gmail.com>
2014-05-09 21:35:06 +0800

    Version 0.3
    - fix a bug that is caused when the colorString is "000"
    - remove unused values and images.
    - rename colornames to prevent conflicts

--- PolljoySDK/src/com/polljoy/util/PJColorHelper.java
+++ PolljoySDK/src/com/polljoy/util/PJColorHelper.java
@@ -1,14 +1,20 @@
 package com.polljoy.util;
 
 import android.graphics.Color;
+import android.util.Log;
 
 public class PJColorHelper {
+	private static final String TAG = "PJColorHelper";
+
 	public static int colorForString(String string) {
 		try {
 			String colorString = "#" + string;
 			int color = Color.parseColor(colorString);
 			return color;
 		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			Log.w(TAG, "Color parsing error from string: " + string);
 			e.printStackTrace();
 		}
 		return 0;
